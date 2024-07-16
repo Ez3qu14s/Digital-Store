3b7fa5c1d36568722006e75872fbd179add8c8a1
Ezequias Bernardo <62806474+Ez3qu14s@users.noreply.github.com>
2024-07-16 13:50:56 -0300

    fix: adicionar o arquivo tailwind.config ao git

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#C92071',
+        secondary: '#B5B6F2',
+        tertiary: '#991956',
+        error: '#EE4266',
+        success: '#52CA76',
+        warning: '#F6AA1C',
+
+        'dark-gray': '#1F1F1F',
+        'dark-gray-2': '#474747',
+        'dark-gray-3': '#666666',
+        'light-gray': '#8F8F8F',
+        'light-gray-2': '#CCCCCC',
+        'light-gray-3': '#F5F5F5',
+      },
+    },
   },
   plugins: [],
 };
