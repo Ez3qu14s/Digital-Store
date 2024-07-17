a868e50d1a91eb810d3e690fc796bba1b7b1cc61
Ezequias Bernardo <62806474+Ez3qu14s@users.noreply.github.com>
2024-07-17 16:10:48 -0300

    feat: nova rota adicionada

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
-
 import Header from './Components/Header';
+import Produtos from './Pages/Producs';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/produtos" element={<Produtos />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
