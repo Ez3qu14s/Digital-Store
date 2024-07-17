33aa77fb2ab6bc80cbd7028c14f9a88f44bb8d56
Daniel Santos <dsaraujo718@gmail.com>
2024-07-16 23:40:30 -0300

    Resolvendo conflito no appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './index.css';
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '/pages/Home/index.jsx';
 
@@ -14,15 +13,6 @@ function App() {
       </BrowserRouter>
     </>
   );
-=======
-import Home from './Pages/Home';
-function App() {
-  return (
-    <div>
-      <Home/>
-    </div>
-  )
->>>>>>> 1b74020 (Adicionando componente emAlta)
 }
 
 export default App;
