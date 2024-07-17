6026fc9e8169b317af60a46b5de74b3ae9f962cb
= <se_oliveira@outlook.com>
2024-07-16 22:18:42 -0300

    fix: correcao do nome da pasta pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home/index.jsx';
+import Home from '/pages/Home/index.jsx';
 
 function App() {
   return (
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Hero from './home-components/Hero';
-import EmDestaque from '../Home/home-components/EmDestaque';
+import EmDestaque from './home-components/EmDestaque';
 
 export default function Home() {
   return (
