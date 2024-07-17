34af3f24ff587a2b14aa8ae19f05dfaa301b1392
Daniel Santos de Araújo <dsaraujo718@gmail.com>
2024-07-17 13:56:57 -0300

    Alterando fonte dos textos

--- src/Pages/Home/home-components/EmAlta.jsx
+++ src/Pages/Home/home-components/EmAlta.jsx
@@ -29,20 +29,22 @@ export default function EmAlta() {
                     <div className="flex flex-col sm:w-[292px] sm:h-[439px]" key={produto.nome}>
                         <div className="bg-[#FFFFFF]  h-[180px] w-[163px] flex flex-col sm:w-[292px] sm:h-[321px]">
                             <div className="bg-[#E7FF86] rounded-[29px] flex items-center justify-center mt-3  mx-2  h-[32px] w-[88px]">
-                                <p className="font-bold text-xs text-dark-gray-2 sm:text-2xl">30% OFF</p>
+                                <p className="font-bold text-[14px] text-dark-gray-2 sm:text-[14px]">30% OFF</p>
                             </div>
                             <div className="w-[124.85px] h-[67.46px] sm:w-[248px] sm:h-[134px]" >
                                 <img src={produto.imagem_url} alt={produto.nome} />
                             </div>
                         </div>
                         <div className="flex flex-col gap-1 mt-2 mb-10">
-                            <p className="text-light-gray font-bold text-xs sm:text-2xl">Tênis</p>
-                            <p className="font-medium text-[14px] sm:text-[24px] sm:font-normal text-dark-gray-2">{produto.nome}</p>
+                            <p className="text-light-gray font-bold text-normal sm:text-2xl">Tênis</p>
+                            <p className="truncate text-ellipsis overflow-hidden ..."></p>
+                            <p className="font-medium text-[14px] truncate sm:text-[24px] sm:font-normal text-dark-gray-2 ">{produto.nome}</p>
+                            
                             <div className="flex gap-3">
-                                <div className="text-light-gray font-normal text-sm line-through sm:text-2xl">
+                                <div className="text-light-gray font-normal text-normal line-through sm:text-2xl">
                                     ${produto.preco_original}
                                 </div>
-                                <div className="text-dark-gray text-xs font-bold sm:text-2xl">
+                                <div className="text-dark-gray text-normal font-bold sm:text-2xl">
                                     ${produto.preco_desconto}
                                 </div>
                             </div>
@@ -50,7 +52,6 @@ export default function EmAlta() {
                     </div>
                 ))}
             </div>
-
         </div>
     )
 }
