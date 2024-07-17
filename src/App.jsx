8e38f701b8ddbf4d92ebd67fb342071abcafcbb8
Daniel Santos <dsaraujo718@gmail.com>
2024-07-16 23:44:22 -0300

    Consertando importação da home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '/pages/Home/index.jsx';
+import Home from './Pages/Home';
+
 
 function App() {
   return (
