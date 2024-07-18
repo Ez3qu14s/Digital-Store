a08f2848cf72a7aa768412bd9b2e25481f484d1f
= <se_oliveira@outlook.com>
2024-07-17 21:48:02 -0300

    refactor: refatoracao da call to action

--- src/Pages/Home/home-components/CallToAction.jsx
+++ src/Pages/Home/home-components/CallToAction.jsx
@@ -3,26 +3,31 @@ import TenisCallToAction from '/public/assets/tenisCallToAction.png';
 
 const CallToAction = () => {
   return (
-    <div className="flex justify-center">
-      <div className="flex flex-col justify-center md:gap-12 md:flex-row p-4  w-[1200px]">
-        <img className="md:w-1/2" src={TenisCallToAction} alt="" />
-        <div className="self-center">
-          <span className="block text-[14px] text-primary font-medium mb-4">
+    <div className="flex justify-center py-4 pt-12">
+      <div className="flex flex-col sm:gap-8 md:flex-row px-4">
+        <img
+          className="self-center w-[420px] lg:w-[520px]"
+          src={TenisCallToAction}
+          alt=""
+        />
+
+        <section className="max-w-[520px] self-center">
+          <span className="text-primary font-medium text-sm">
             Oferta especial
           </span>
-          <h1 className="text-[28px] lg:text-[48px] leading-[32px] lg:leading-[50px] font-bold">
-            Air Jordan edição de <br className="hidden md:block" /> colecionador
+          <h1 className="my-2 font-bold text-3xl text-dark-gray-2">
+            Air Jordan edição de colecionador
           </h1>
-          <p className="text-[14px] md:text-[16px] leading-[22px] md:leading-[28px] my-3">
+          <p className="text-dark-gray-2">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip
           </p>
-          <button className="w-[200px] p-2 mt-4 bg-primary text-white rounded-md">
-            Ver Oferta
+          <button className="w-[200px] py-2 bg-primary my-3 rounded-md font-medium text-light-gray-3">
+            Ver oferta
           </button>
-        </div>
+        </section>
       </div>
     </div>
   );
