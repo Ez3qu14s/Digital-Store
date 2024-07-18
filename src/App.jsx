0839b14f79c217c7b52e2febf08d43ad1e27748c
Daniel Santos <dsaraujo718@gmail.com>
2024-07-17 23:56:28 -0300

    Appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import Home from './Pages/Home'
 import Products from './Pages/Products';
 import Product from './Pages/Product'
@@ -13,10 +12,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-
           <Route path="/produtos" element={<Products />} />
           <Route path='/produto/:nome' element={<Product/>}/>
-
         </Routes>
       </BrowserRouter>
     </>
