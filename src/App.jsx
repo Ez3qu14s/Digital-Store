0fb1f6a89406961b8413919fe55120f553c333c1
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2024-07-19 15:20:41 -0300

    fix: tinha dado tela branca

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home'
 import Produtos from './Pages/Products'
 import Produto from './Pages/Product'
-import Login from './Pages/Login'
-import Cadastro from './Pages/Cadastro'
-import FormularioCadastro from './Pages/FormularioCadastro'
-
 
 function App() {
   return (
@@ -17,9 +13,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/produtos" element={<Produtos />} />
           <Route path='/produto/:id' element={<Produto />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/cadastro' element={<Cadastro />} />
-          <Route path='/formulario' element={<FormularioCadastro />} />
         </Routes>
       </BrowserRouter>
     </>
